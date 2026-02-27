04391000cf22cb1f80e7f0f7b54c5f0c835ab09b
Gia <gia.pereira31@gmail.com>
2026-02-27 14:05:14 +0000

    Configure for Heroku deployment

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+primaryColor = '#1f77b4'\n\
 " > ~/.streamlit/config.toml
